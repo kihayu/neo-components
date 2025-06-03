61e867baf8a09e5bb55c4fd54a90500fe8e4de11
Keanu Hie <hie.keanu@gmail.com>
2025-06-03 09:29:43 +0200

    feat: implement refactored acordion component

---
+++ src/components/ui/accordion/NeoAccordionItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <AccordionItem data-slot="accordion-item" v-bind="forwardedProps" :class="props.class">
+    <slot />
+  </AccordionItem>
+</template>
+
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+import { AccordionItem, type AccordionItemProps, useForwardProps } from 'reka-ui'
+
+const props = defineProps<AccordionItemProps & { class?: HTMLAttributes['class'] }>()
+
+const delegatedProps = reactiveOmit(props, 'class')
+
+const forwardedProps = useForwardProps(delegatedProps)
+</script>
---
+++ src/components/ui/accordion/index.ts
@@ -0,0 +1,4 @@
+export { default as NeoAccordion } from './NeoAccordion.vue'
+export { default as NeoAccordionContent } from './NeoAccordionContent.vue'
+export { default as NeoAccordionItem } from './NeoAccordionItem.vue'
+export { default as NeoAccordionTrigger } from './NeoAccordionTrigger.vue'
